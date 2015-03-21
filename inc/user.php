3e596f9c41a6229f5535340850dbfbfe3afe5f63
John Wiebe <johnjw59@gmail.com>
2015-03-21 02:52:47 -0700

    you can now register an account!!!

--- inc/user.php
+++ inc/user.php
@@ -9,10 +9,27 @@ class User extends Repo
     public function save($review) { /* ... */ }
     public function byId($id) { /* ... */ }
 
-    public function login($email, $pass) 
-    { 
+    public function login($email, $pass) { 
         $query = $this->prepare('SELECT user_id FROM user WHERE email=:email AND password=:pass');
         $query->execute(array('email' => $email, 'pass' => $pass));
         return $query->fetch(PDO::FETCH_CLASS);
+    }
+
+    public function register($email, $name, $pass) {
+        try {
+            $query = $this->prepare('INSERT INTO user (name, email, password) VALUES (:name, :email, :pass)');
+            $query->execute(array('email' => $email, 'name' => $name, 'pass' => $pass));
+        }
+        catch (PDOException $e) {
+            if ($e->getCode() == '23000') {
+                return FALSE;
+            }
+            else {
+                throw $e;
+            }
+        }
+        $query = $this->prepare('SELECT LAST_INSERT_ID()');
+        $query->execute();
+        return $query->fetch(PDO::FETCH_NUM);
     }
 }
