55f88ad92316b2949e4965a6dab33cef8852e36b
John Wiebe <johnjw59@gmail.com>
2015-03-21 22:19:46 -0700

    changed wording for account on navbar

--- inc/navBar.php
+++ inc/navBar.php
@@ -3,7 +3,7 @@
 	<div class="button"><a href="index.php?page=game">Games</a></div>
 	<div class="button"><a href="index.php?page=developer">Developers</a></div>
   <?php if(!empty($_SESSION['user_id'])): ?>
-	  <div class="button"><a href="index.php?page=user">User</a></div>
+	  <div class="button"><a href="index.php?page=user">Your Account</a></div>
     <div class="button"><a href="index.php?logout">Log out</a></div>
   <?php else: ?>
     <div class="button"><a href="index.php?page=user">Login</a></div>
